c158fc422f5966ae7dab3e2dfd081061ead1bd1a
Dillon Ellis <dellis@fcul.com>
2022-05-24 15:19:01 -0400

    original cufs link page

---
+++ cufs-link.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="CUFS was established in 1993 and is currently owned by four Georgia Credit Unions. The Credit Unions created CUFS to provide mortgages for members seeking affordable lending solutions and trustworthy, expert advice.">
+    <meta name="keywords" content="CUFS, Credit Union Financial Services, mortgage, home equity, Mortgage Loans, Home Loans">
+
+    <title>CUFS | Mortgage Options</title>
+
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+
+    <!-- SLIDER -->
+    <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />    
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+    <link rel="stylesheet" type="text/css" href="css/carousel.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Remodal styles -->
+    <link rel="stylesheet" href="remodal/dist/remodal.css">
+    <link rel="stylesheet" href="remodal/dist/remodal-default-theme.css">
+
+    <!-- COLORS -->
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+	
+	<script language="javascript">
+if (document.location.protocol != "https:")
+{
+document.location.href = "https://www.cufs.org" + document.location.pathname;
+};
+</script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>  
+
+    <div class="wrapper">
+        <div class="topbar">
+            <?php include 'inc/topbar.php';?>
+        </div><!-- end topbar -->
+
+        <header class="header header-white">
+            <div class="container-fluid">
+                <div class="row mb-5">
+                    <div class="col-md-12">
+                        <?php include 'inc/nav.php';?>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </header>
+
+        <section class="page-title grey clearfix">
+            <div class="container">
+                <div class="row">  
+                    <div class="col-md-12 text-left">
+                        <h1>CUFS LINK</h1>
+                        <p>Pre-Qualify for a Mortgage Purchase or Refinance</p>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section><!-- end section -->
+
+        <section class="section white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="text-widget list-styles pb20">
+							<p>Please select the Licensed Mortgage Advisor you would like to use from the list below:</p>
+							 <div class="row">
+					 
+					 
+					 <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="team-member">
+                          <div class="entry">
+                                <img src="images/staff/debra-sibley.jpg" alt="Debra Sibley">
+                                <div class="magnifier">
+                                    <div class="visible-buttons">
+                                        <a href="https://hosting.bytesoftware.com/apps/BCP_40583?LO=DebraSibley" target="_blank"><i class="fa fa-pencil-square-o"></i><br><strong style="color: #ffffff;">Apply with.</strong></a>
+                                    </div><!-- end buttons -->
+                                </div><!-- end magnifier -->
+                            </div><!-- end entry -->
+                          <h3 align="center"><a href="https://hosting.bytesoftware.com/apps/BCP_40583?LO=DebraSibley" target="_blank">Debra Sibley</a></h3> 
+                             <p>&nbsp;</p>
+                            
+                        </div><!-- end team-member -->
+                    </div><!-- end col -->
+								 <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="team-member">
+                            <div class="entry">
+                                <img src="images/staff/whitney-cannon.jpg" alt="Whitney Cannon">
+                                <div class="magnifier">
+                                    <div class="visible-buttons">
+                                        <a href="https://hosting.bytesoftware.com/apps/BCP_40583?LO=Whitney%20Cannon" target="_blank"><i class="fa fa-pencil-square-o"></i><br><strong style="color: #ffffff;">Apply with.</strong></a>
+                                  </div><!-- end buttons -->
+                              </div><!-- end magnifier -->
+                          </div><!-- end entry -->
+                          <h3 align="center"> <a href="https://hosting.bytesoftware.com/apps/BCP_40583?LO=Whitney%20Cannon" target="_blank" >Whitney Cannon</a></h3>
+                          <p>&nbsp;</p>
+                          
+                        </div><!-- end team-member -->
+                    </div><!-- end col -->
+								 
+								 <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="team-member">
+                            <div class="entry">
+                                <img src="images/staff/mark-agee.jpg" alt="Mark Agee">
+                                <div class="magnifier">
+                                    <div class="visible-buttons">
+                                        <a href="https://hosting.bytesoftware.com/apps/BCP_40583/?LO=MarkAgee" target="_blank"><i class="fa fa-pencil-square-o"></i><br><strong style="color: #ffffff;">Apply with.</strong></a>
+                                  </div><!-- end buttons -->
+                              </div><!-- end magnifier -->
+                          </div><!-- end entry -->
+                          <h3 align="center"> <a href="https://hosting.bytesoftware.com/apps/BCP_40583/?LO=MarkAgee" target="_blank">Mark Agee</a></h3>
+                          <p>&nbsp;</p>
+                          
+                        </div><!-- end team-member -->
+                    </div><!-- end col -->
+								 
+                </div><!-- end row -->
+							
+							 <div class="row">
+								 
+								 <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="team-member">
+                            <div class="entry">
+                                <img src="images/staff/ken-obiecunas.jpg" alt="Ken Obiecunas">
+                                <div class="magnifier">
+                                    <div class="visible-buttons">
+                                        <a href="https://hosting.bytesoftware.com/apps/BCP_40583?LO=Ken Obiecunas" target="_blank"><i class="fa fa-pencil-square-o"></i><br><strong style="color: #ffffff;">Apply with.</strong></a>
+                                  </div><!-- end buttons -->
+                              </div><!-- end magnifier -->
+                          </div><!-- end entry -->
+                          <h3 align="center"> <a href="https://hosting.bytesoftware.com/apps/BCP_40583?LO=KenObiecunas" target="_blank">Ken Obiecunas</a></h3>
+                          <p>&nbsp;</p>
+                          
+                        </div><!-- end team-member -->
+                    </div><!-- end col -->
+					 
+					 <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="team-member">
+                          <div class="entry">
+                                <img src="images/staff/i-dont-know.jpg" alt="I don't know">
+                                <div class="magnifier">
+                                    <div class="visible-buttons">
+                                        <a href="https://hosting.bytesoftware.com/apps/BCP_40583" target="_blank"><i class="fa fa-pencil-square-o"></i><br><strong style="color: #ffffff;">Apply with.</strong></a>
+                                  </div><!-- end buttons -->
+                            </div><!-- end magnifier -->
+                          </div><!-- end entry -->
+                         <h3 align="center"><a href="https://hosting.bytesoftware.com/apps/BCP_40583" target="_blank">I don't know yet</a></h3> 
+                             <p>&nbsp;</p>
+                            
+                        </div><!-- end team-member -->
+                    </div><!-- end col -->
+								 
+								  
+					 
+								 
+								 
+                </div><!-- end row -->                        </div>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+
+            </div><!-- end container -->
+        </section><!-- end section -->
+
+
+        <footer class="section footer grey">
+            <?php include 'inc/footerT.php';?>
+        </footer><!-- end footer -->
+
+        <footer class="copyrights">
+            <?php include 'inc/footerB.php';?>
+        </footer><!-- end copyrights -->
+
+    </div><!-- end wrapper -->
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/parallax.js"></script>
+    <script src="js/retina.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/carousel.js"></script>
+    <script src="js/carousel-plugins.js"></script>
+    <script src="js/custom.js"></script>
+
+    <script src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+    <script src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+	
+	<script src="remodal/dist/remodal.js"></script>
+<?php include 'inc/modals.php';?>
+</body>
+</html>
