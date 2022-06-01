a44559a61ec708fb27b21733a40270192ede268d
Dillon Ellis <dellis@fcul.com>
2022-06-01 13:18:53 -0400

    original our team page

---
+++ our-team.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="CUFS was established in 1993 and is currently owned by four Georgia Credit Unions. The Credit Unions created CUFS to provide mortgages for members seeking affordable lending solutions and trustworthy, expert advice.">
+    <meta name="keywords" content="CUFS, Credit Union Financial Services, mortgage, home equity, Mortgage Loans, Home Loans">
+
+    <title>CUFS | Our Team</title>
+
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+
+    <!-- SLIDER -->
+    <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />    
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+    <link rel="stylesheet" type="text/css" href="css/carousel.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Remodal styles -->
+    <link rel="stylesheet" href="remodal/dist/remodal.css">
+    <link rel="stylesheet" href="remodal/dist/remodal-default-theme.css">
+
+    <!-- COLORS -->
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+	
+	<script language="javascript">
+if (document.location.protocol != "https:")
+{
+document.location.href = "https://www.cufs.org" + document.location.pathname;
+};
+</script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>  
+
+    <div class="wrapper">
+        <div class="topbar">
+            <?php include 'inc/topbar.php';?>
+        </div><!-- end topbar -->
+
+        <header class="header header-white">
+            <div class="container-fluid">
+                <div class="row mb-5">
+                    <div class="col-md-12">
+                        <?php include 'inc/nav.php';?>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </header>
+
+        <section class="page-title grey clearfix">
+            <div class="container">
+                <div class="row">  
+                    <div class="col-md-12 text-left">
+                        <h1>Our Team</h1>
+                        <p>Get To Know Us</p>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section><!-- end section -->
+
+        <section class="section white">
+            <div class="container">
+				<div class="row">
+                    <div class="col-md-12">
+                        <div class="text-widget pb20">
+							<p>Welcome to Credit Union Financial Services - <strong>CUFS</strong> - the mortgage company for Credit Union Members!</p>
+							<p class="pt20"><strong>CUFS</strong> is owned by four Georgia credit unions. These credit unions joined together to provide mortgage services for members who were seeking affordable and trustworthy mortgage products. They wanted a mortgage company that had members' best interest in mind when securing a Home Mortgage and at the same time, considered the best fit for members' financial goals and their future financial security.</p>
+							<p class="pt20"><strong>Our Licensed Mortgage Advisors & Mortgage Servicing Staff:</strong></p>
+					  </div>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+                
+                 <div class="row">
+					 
+					 
+					 <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="team-member">
+                            <div class="entry">
+                                <img src="images/staff/debra-sibley.jpg" alt="Debra Sibley">
+                                <div class="magnifier">
+                                    <div class="visible-buttons">
+                                        <a href="https://hosting.bytesoftware.com/apps/BCP_40583?LO=DebraSibley" target="_blank"><i class="fa fa-pencil-square-o"></i><br><strong style="color: #ffffff;">Apply with.</strong></a>
+                                    </div><!-- end buttons -->
+                                </div><!-- end magnifier -->
+                            </div><!-- end entry -->
+                            <h3>Debra Sibley</h3> 
+                            <small>Licensed Mortgage Advisor</small>
+                            <p>NMLS License #713302<br>
+678-658-4476 Direct</p>
+                            <div>
+                                <a href="#" data-remodal-target="debra" class="btn btn-primary btn-sm" style="color: #ffffff;">About</a>
+							    <a href="https://hosting.bytesoftware.com/apps/BCP_40583?LO=DebraSibley" target="_blank" class="btn btn-default btn-sm">Apply With</a>
+                            </div>
+                        </div><!-- end team-member -->
+                    </div><!-- end col -->
+
+					
+					 <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="team-member">
+                            <div class="entry">
+                                <img src="images/staff/mark-agee.jpg" alt="Mark Agee">
+								<div class="magnifier">
+                                    <div class="visible-buttons">
+                                         <a href="https://hosting.bytesoftware.com/apps/BCP_40583/?LO=MarkAgee" target="_blank"><i class="fa fa-pencil-square-o"></i><br><strong style="color: #ffffff;">Apply with.</strong></a>
+                                  </div><!-- end buttons -->
+                              </div><!-- end magnifier -->
+                            </div><!-- end entry -->
+                            <h3>Mark Agee</h3> 
+                            <small>Licensed Mortgage Advisor</small>
+                            <p>NMLS License #273301<br>
+678-712-4485 Direct</p>
+                            <div>
+								<a href="#" data-remodal-target="agee" class="btn btn-primary btn-sm" style="color: #ffffff;">About</a>
+							    <a href="https://hosting.bytesoftware.com/apps/BCP_40583/?LO=MarkAgee" target="_blank" class="btn btn-default btn-sm">Apply With</a>
+                            </div>
+                        </div><!-- end team-member -->
+                    </div><!-- end col -->
+					 
+					 
+					  <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="team-member">
+                            <div class="entry">
+                                <img src="images/staff/ken-obiecunas.jpg" alt="Ken Obiecunas">
+								<div class="magnifier">
+                                    <div class="visible-buttons">
+                                         <a href="https://hosting.bytesoftware.com/apps/BCP_40583?LO=Ken Obiecunas" target="_blank"><i class="fa fa-pencil-square-o"></i><br><strong style="color: #ffffff;">Apply with.</strong></a>
+                                  </div><!-- end buttons -->
+                              </div><!-- end magnifier -->
+                            </div><!-- end entry -->
+                            <h3>Ken Obiecunas</h3> 
+                            <small>Production Manager</small>
+                            <p>NMLS License #575720<br>
+412-260-5004 Direct</p>
+                            <div>
+								<a href="#" data-remodal-target="ken" class="btn btn-primary btn-sm" style="color: #ffffff;">About</a>
+							    <a href="https://hosting.bytesoftware.com/apps/BCP_40583?LO=KenObiecunas" target="_blank" class="btn btn-default btn-sm">Apply With</a>
+                            </div>
+                        </div><!-- end team-member -->
+                    </div><!-- end col -->
+
+
+                </div><!-- end row -->
+				
+				<p>&nbsp;</p>
+				
+				<div class="row">
+                    <div class="col-md-12">
+                        <div class="text-widget pb20">
+							<p class="pt20"><strong>Our Licensed Management Team:</strong></p>
+					  </div>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+                
+                 <div class="row">
+					 
+					 <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="team-member">
+                            <div class="entry">
+                                <img src="images/staff/placeholder.png" alt="Michelle Wright">
+                            </div><!-- end entry -->
+                            <h3>Michelle Wright</h3> 
+                            <small>Servicing Manager</small>
+                            <p>NMLS License #787995<br>
+678-658-4472 Direct</p>
+                            <div>
+								<!-- <a href="#" data-remodal-target="michelle" class="btn btn-primary btn-sm" style="color: #ffffff;">About</a> -->
+                            </div>
+                        </div><!-- end team-member -->
+                    </div><!-- end col -->
+					 
+					
+                </div><!-- end row -->
+				
+					
+            </div><!-- end container -->
+        </section><!-- end section -->
+
+
+        <footer class="section footer grey">
+            <?php include 'inc/footerT.php';?>
+        </footer><!-- end footer -->
+
+        <footer class="copyrights">
+            <?php include 'inc/footerB.php';?>
+        </footer><!-- end copyrights -->
+
+    </div><!-- end wrapper -->
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/parallax.js"></script>
+    <script src="js/retina.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/carousel.js"></script>
+    <script src="js/carousel-plugins.js"></script>
+    <script src="js/custom.js"></script>
+
+    <script src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+    <script src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+	
+	<script src="remodal/dist/remodal.js"></script>
+<?php include 'inc/modals.php';?>
+</body>
+</html>
