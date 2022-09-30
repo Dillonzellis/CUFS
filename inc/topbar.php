599e36564e0f6d40b88357124f15fa538e63c7b9
Dillon Ellis <dellis@fcul.com>
2022-09-30 12:24:51 -0400

    original top bar

---
+++ inc/topbar.php
@@ -0,0 +1,37 @@
+<div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="top-details">
+                            <strong>Questions?</strong>
+                            <span><i class="fa fa-envelope-o"></i> <a href="mailto:info@cufs.org" style="text-decoration: underline !important;">info@cufs.org</a></span>
+                            <span><i class="fa fa-phone"></i>  (800) 503-6855</span>
+							<span><a href="https://www.linkedin.com/company/54663670/admin/" target="_blank"><img src="../images/linkedin.png" alt="LinkedIn"></a></span>
+							<span><a href="https://www.facebook.com/CUFSLLC" target="_blank"><img src="../images/facebookSM.png" alt="Facebook"></a></span>
+							<span><a href="https://youtu.be/5Q5_4aCrjEI" target="_blank"><img src="../images/youtube.png" alt="YouTube"></a></span>
+                        </div>
+                    </div><!-- end left -->
+                    <div class="col-md-6 text-right">
+
+
+                        <div class="btn-group">
+							<a href="https://cufs.estatusconnect.com/" target="_blank" class="btn btn-primary-green btn-lg" style="margin-right: 25px;">
+								<span style="text-transform: lowercase;">e</span>Status
+							</a> 
+							<a href="https://www.cufs.org/cufs-link.php" class="btn btn-primary-blue btn-lg" style="    border-color: black!important;
+    padding: .5em !important;
+    font-weight: 600;
+    line-height: 21px;
+    background: #ff0000;
+    background: -moz-linear-gradient(top, #000000 0%, #ffffff 2%, #ff0000 3%, #ff0000 48%, #ff0000 55%, #ff0000 97%, #ffffff 98%, #000000 100%);
+    background: -webkit-linear-gradient(top, #333333 0%,#ffffff 2%,#ff0000 3%,#ff0000 48%,#ff0000 55%,#ff0000 97%,#ffffff 98%,#333333 100%);
+    background: linear-gradient(to bottom, #333333 0%,#ffffff 2%,#ff0000 3%,#ff0000 48%,#ff0000 55%,#ff0000 97%,#ffffff 98%,#333333 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#000000', endColorstr='#000000',GradientType=0 );
+    border-right-width: 1px;
+    border-right-style: solid;">
+								New Mortgage Request<br>
+								Click Here
+							</a>
+                        </div><!-- /btn-group -->
+                    </div><!-- end left -->
+                </div><!-- end row -->
+            </div><!-- end container -->
---
+++ index2.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <title>CUFS | Home</title>
+
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+
+    <!-- SLIDER -->
+    <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+    <link rel="stylesheet" type="text/css" href="css/carousel.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- Remodal styles -->
+    <link rel="stylesheet" href="remodal/dist/remodal.css">
+    <link rel="stylesheet" href="remodal/dist/remodal-default-theme.css">
+
+    <!-- COLORS -->
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+
+    <script language="javascript">
+        if (document.location.protocol != "https:") {
+            document.location.href = "https://www.cufs.org" + document.location.pathname;
+        };
+    </script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="wrapper">
+		
+        <style>
+            .banner {
+                background: #124092;
+                text-align: center;
+                padding: 20px 5px;
+                border: 4px solid #89c540;
+            }
+
+            .banner p {
+                color: white;
+                font-size: 20px;
+            }
+
+            .banner p strong {
+                font-size: 32px;
+                color: white;
+            }
+
+            .notification-bar{
+                font-size: 20px;
+                background-color: yellow;
+                color: #fff;
+            }
+        </style>
+       
+        <div class="topbar">
+            <?php include 'inc/topbar.php'; ?>
+        </div><!-- end topbar -->
+
+        <header class="header header-white">
+            <div class="container-fluid">
+                <div class="row mb-5">
+                    <div class="col-md-12">
+                        <?php include 'inc/nav.php'; ?>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </header>
+
+        <div class="notification-bar">
+            <strong>Our offices will be closed on Monday, October 10th for Columbus Day.  We will re-open on Tuesday, October 11th at 8 AM.</strong>
+        </div>
+
+        <div class="slider-section">
+            <div class="tp-banner-container">
+                <div class="tp-banner">
+                    <ul>
+                        <li data-transition="fade" data-slotamount="1" data-masterspeed="500" data-thumb="images/banner/cufs-9-30-22.png" data-saveperformance="off" data-title="Vet">
+                            <img src="images/banner/cufs-9-30-22.png" alt="fullslide1" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
+                            <div class="tp-caption slider_layer_01 lft tp-resizeme" data-y="100" data-x="0" data-speed="1000" data-start="600" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="1000" style="z-index: 9; max-width: auto; max-height: auto; white-space: nowrap;">Discover the Credit<br>Union Financial<br>Services Difference
+                            </div>
+                            <div class="tp-caption slider_layer_02 lft tp-resizeme" data-y="360" data-x="10" data-speed="1600" data-start="1000" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="1000" style="z-index: 9; max-width: auto; max-height: auto; white-space: nowrap;">Providing homeownership solutions and helping credit union<br>members reach personal goals, one member at a time, while building<br>strong partnerships with excellent customer service and integrity.
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div><!-- end SLIDER -->
+
+        <section class="section grey">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-12">
+                        <div class="service-box noradius" style="text-align: center;">
+                            <p><a href="https://cufs.estatusconnect.com/index.html#/home/login" target="_blank"><img src="images/estatus-logo.png" alt="e status logo image"></a></p>
+                            <h3><a href="https://cufs.estatusconnect.com/index.html#/home/login" target="_blank" style="color: #000000!important;">Account Info/Make a Payment</a></h3>
+                        </div><!-- end service-box -->
+                    </div><!-- end col -->
+                    <div class="col-lg-4 col-md-4 col-sm-12">
+                        <div class="service-box noradius" style="text-align: center;">
+                            <p><a href="https://www.cufs.org/cufs-link.php" target="_blank"><img src="images/cufslink-logo.png" alt="CUFS Link"></a></p>
+                            <h3><a href="https://www.cufs.org/cufs-link.php" target="_blank" style="color: #000000!important;">Apply Online Now</a></h3>
+                        </div><!-- end service-box -->
+                    </div><!-- end col -->
+                    <div class="col-lg-4 col-md-4 col-sm-12">
+                        <div class="service-box noradius" style="text-align: center;">
+                            <p><a href="https://www.cufs.org/our-team.php"><img src="images/ourteam-icon.png" alt="Icon of a group of people"></a></p>
+                            <h3><a href="https://www.cufs.org/our-team.php" style="color: #000000!important;">Meet Our Team</a></h3>
+                        </div><!-- end service-box -->
+                    </div><!-- end col -->
+                </div><!-- end row -->
+
+            </div><!-- end container -->
+        </section><!-- end section -->
+
+        <section class="section white">
+            <div class="container">
+                <div class="big-title text-center">
+                    <h3>Today's Featured <span>Rates</span></h3>
+                    <hr>
+                    <p class="lead">All interest rates displayed are subject to change. Please contact a Mortgage Consultant for current rates. Your actual interest rate will be determined at the time an interest rate lock is completed.</p>
+                </div><!-- end title -->
+
+                <hr class="invis">
+
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <div class="text-widget pb20" style="align-content: center; text-align: center;">
+                            <a href="rates.php" alt="View all rates" class="btn btn-primary-green btn-lg" style="margin-right: 25px;">
+                                View All Rates
+                            </a>
+                        </div><!-- end service-box -->
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section><!-- end section -->
+
+
+        <footer class="section footer grey">
+            <?php include 'inc/footerT.php'; ?>
+        </footer><!-- end footer -->
+
+        <footer class="copyrights">
+            <?php include 'inc/footerB.php'; ?>
+        </footer><!-- end copyrights -->
+
+    </div><!-- end wrapper -->
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/parallax.js"></script>
+    <script src="js/retina.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/progress.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/carousel.js"></script>
+    <script src="js/carousel-plugins.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/typetext.js"></script>
+
+    <script src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+    <script src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+    <script src="remodal/dist/remodal.js"></script>
+
+    <script type="text/javascript">
+        /* ==============================================
+        SLIDER
+        =============================================== */
+
+        jQuery('.tp-banner').show().revolution({
+            dottedOverlay: "none",
+            delay: 16000,
+            startwidth: 1170,
+            startheight: 900,
+            hideThumbs: 200,
+            thumbWidth: 100,
+            thumbHeight: 50,
+            thumbAmount: 5,
+            navigationType: "none",
+            navigationArrows: "solo",
+            navigationStyle: "round",
+            touchenabled: "on",
+            onHoverStop: "on",
+            swipe_velocity: 0.7,
+            swipe_min_touches: 1,
+            swipe_max_touches: 1,
+            drag_block_vertical: false,
+            parallax: "mouse",
+            parallaxBgFreeze: "on",
+            parallaxLevels: [7, 4, 3, 2, 5, 4, 3, 2, 1, 0],
+            keyboardNavigation: "off",
+            navigationHAlign: "center",
+            navigationVAlign: "bottom",
+            navigationHOffset: 0,
+            navigationVOffset: 20,
+            soloArrowLeftHalign: "left",
+            soloArrowLeftValign: "center",
+            soloArrowLeftHOffset: 20,
+            soloArrowLeftVOffset: 0,
+            soloArrowRightHalign: "right",
+            soloArrowRightValign: "center",
+            soloArrowRightHOffset: 20,
+            soloArrowRightVOffset: 0,
+            shadow: 0,
+            fullWidth: "on",
+            fullScreen: "off",
+            spinner: "spinner4",
+            stopLoop: "off",
+            stopAfterLoops: -1,
+            stopAtSlide: -1,
+            shuffle: "off",
+            autoHeight: "off",
+            forceFullWidth: "off",
+            hideThumbsOnMobile: "off",
+            hideNavDelayOnMobile: 1500,
+            hideBulletsOnMobile: "off",
+            hideArrowsOnMobile: "off",
+            hideThumbsUnderResolution: 0,
+            hideSliderAtLimit: 0,
+            hideCaptionAtLimit: 0,
+            hideAllCaptionAtLilmit: 0,
+            startWithSlide: 0
+        });
+    </script>
+    <?php include 'inc/modals.php'; ?>
+</body>
+
+</html>
